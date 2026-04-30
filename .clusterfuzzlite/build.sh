32270fffc7c84ecec43dbd0c62f2cfda15d783b0
georgengyn <g.nguyen@wustl.edu>
2026-04-29 22:43:00 -0500

    fixed shell script for fuzzing

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -eu
 
+# Compile shared library from src/filesystem.cpp
 $CXX $CXXFLAGS -I include -c src/filesystem.cpp -o filesystem.o
 
-ar rcs libmockfs.a lib.o filesystem.o
+ar rcs libmockfs.a filesystem.o
 
+# Build the fuzz target, linking against the shared library and fuzzing engine
 $CXX $CXXFLAGS -I include \
     fuzz/fuzz_display_file.cpp \
     -o $OUT/fuzz_display_file \
     libmockfs.a \
-    $LIB_FUZZING_ENGINENGINE
+    $LIB_FUZZING_ENGINE
